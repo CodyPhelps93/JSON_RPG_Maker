0c98ff1543ffe6d9f6233f7804d00954f1ce0662
CodyPhelps93 <codyphelps1993@yahoo.com>
2025-09-01 00:37:56 -0500

    Added Lua script support for player commands. Commands now execute Lua scripts, allowing for more complex and dynamic game interactions.

---
+++ JSON_RPG_Maker/GameCommand.cs
@@ -0,0 +1,9 @@
+namespace JSON_RPG_Maker
+{
+    public class GameCommand
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string LScript { get; set; }
+    }
+}
